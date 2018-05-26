10aff7dc535bd5568c537dd7933a90f9f0fb5d9f
Aaron Wilder <element1028@gmaill.com>
2018-05-26 00:48:57 -0700

    added personal bookmarks

--- bookmarks/admin.py
+++ bookmarks/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Bookmark
+from .models import Bookmark, PersonalBookmark
 
-admin.site.register(Bookmark)
+admin.site.register((Bookmark, PersonalBookmark))
--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from uuid import uuid4
 
 class Bookmark(models.Model):
@@ -8,3 +9,6 @@ class Bookmark(models.Model):
   notes = models.TextField(blank=True)
   created_at = models.DateTimeField(auto_now_add=True)
   last_modified = models.DateTimeField(auto_now=True)
+
+class PersonalBookmark(Bookmark):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
