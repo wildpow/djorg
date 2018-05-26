b2251504e1adf6d7ffe0224802fc74bc81ae3b4a
Aaron Wilder <element1028@gmaill.com>
2018-05-25 22:00:33 -0700

    added fields for bookmarks and ran migrations

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+class Bookmark(models.Model):
+  id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+  url = models.URLField('URL', unique=True)
+  name = models.CharField(max_length=200)
+  notes = models.TextField(blank=True)
--- djorg/settings.py
+++ djorg/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bookmarks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
