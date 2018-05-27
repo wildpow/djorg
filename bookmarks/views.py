4bd10b61dcfbf0bd969d9f5229c057815ca34d89
Aaron Wilder <element1028@gmaill.com>
2018-05-27 13:38:00 -0700

    added bookmark view and excluded personal bookmarks from regular bookmarks

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
-from .models import Bookmark
+from .models import Bookmark, PersonalBookmark
 # Create your views here.
 
 def index(request):
-  context = {
-    'bookmarks': Bookmark.objects.all()
-  }
+  context = {}
+
+  pbid = PersonalBookmark.objects.values_list('id')
+
+  context['bookmarks'] = Bookmark.objects.exclude(id__in=pbid)
+
+  if request.user.is_anonymous:
+    context['personal_bookmarks'] = PersonalBookmark.objects.none()
+  else:
+    context['personal_bookmarks'] = PersonalBookmark.objects.filter(user=request.user)
 
   return render(request, 'bookmarks/index.html', context)
