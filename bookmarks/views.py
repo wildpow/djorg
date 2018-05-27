458615f88d29a2fb124de2e516b03a43175bfb94
Aaron Wilder <element1028@gmaill.com>
2018-05-27 14:01:03 -0700

    added new bookmark form

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Bookmark, PersonalBookmark
+from .forms import BookmarkForm
 # Create your views here.
 
 def index(request):
@@ -13,5 +14,7 @@ def index(request):
     context['personal_bookmarks'] = PersonalBookmark.objects.none()
   else:
     context['personal_bookmarks'] = PersonalBookmark.objects.filter(user=request.user)
+
+  context['form'] = BookmarkForm
 
   return render(request, 'bookmarks/index.html', context)
