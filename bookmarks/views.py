390f062e3c3d98a955b9a8b2947b27a0196e796e
Aaron Wilder <element1028@gmaill.com>
2018-05-27 22:10:03 -0700

    hello

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -4,6 +4,15 @@ from .forms import BookmarkForm
 # Create your views here.
 
 def index(request):
+  # import pdb; pdb.set_trace()
+  if request.method == 'POST':
+    form = BookmarkForm(request.POST)
+    if form.is_valid():
+      #TODO check for request.user to allow for personal bookmarks
+      form.save()
+    else:
+      # TODO error
+      pass
   context = {}
 
   pbid = PersonalBookmark.objects.values_list('id')
