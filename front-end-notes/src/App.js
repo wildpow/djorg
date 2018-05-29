d351dec7b91d970de69aee83e544790a424c2225
Aaron Wilder <element1028@gmaill.com>
2018-05-29 16:58:01 -0700

    React front-end up and running

--- front-end-notes/src/App.js
+++ front-end-notes/src/App.js
@@ -2,10 +2,31 @@ import React, { Component } from 'react';
 
 
 class App extends Component {
+  state = { stuff: []}
+  componentWillMount() {
+    fetch('http://127.0.0.1:8000/api/notes/')
+      .then((stuff) => {
+       return stuff.json();
+      })
+      .then((poop) => {
+        this.setState({
+          stuff: poop
+        })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
   render() {
     return (
       <div>
-        
+       {this.state.stuff.map((item, index) => (
+         <div key={index}>
+          {item.title}
+          {item.content}
+         </div>
+       ))}
+        {console.log(this.state.stuff)}
       </div>
     );
   }
